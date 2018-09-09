b490e5cf96dc78742935e1b61a1f6fbfea554a73
Denis Chukanov <Chudzzi@gmail.com>
2018-09-09 16:51:13 +0300

    add sample of dynamic copying of dependencies

---
+++ src/Tests/DockerSample.Dependency/HelloDocker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DockerSample.Dependency
+{
+   public  class HelloDocker
+    {
+        public string Get()
+        {
+            return "Hello from Docker Dependency Sample";
+        }
+    }
+}
